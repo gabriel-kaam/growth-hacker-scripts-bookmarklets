dd20313f3a2e541d7d93b0e443310339092067a0
Gabriel Kaam <gabriel@kaam.fr>
2022-07-10 01:21:42 +0200

    Rename folders

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var path = require('path');
 var out_file = "build/bookmarklets.html";
 var bookmarks = {};
 
+const folder = 'bookmarklets';
+
 function handle_thing(file_or_directory) {
   return fs.statSync(file_or_directory).isFile() ? handle_file(file_or_directory) : handle_directory(file_or_directory);
 }
@@ -15,6 +17,7 @@ function parse_js_code(code) {
 
 function handle_file(file) {
   var data = fs.readFileSync(file, 'utf8');
+  file.substring(folder.length + 1);
   bookmarks[file] = parse_js_code(data);
 }
 
@@ -30,7 +33,7 @@ function handle_directory(directory, callback) {
 
 var jquery_code = '';
 
-handle_directory("bookmarklets");
+handle_directory(folder);
 
 var html = netscape(bookmarks);
 fs.writeFile(out_file, html, function() {
