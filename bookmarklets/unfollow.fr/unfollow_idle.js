33512e8000bab483539df61f5ed3fad10429ab4d
Gabriel Kaam <gabriel@kaam.fr>
2024-09-02 00:23:22 +0200

    Remove unfollow.fr

--- bookmarklets/unfollow.fr/unfollow_idle.js
+++
@@ -1,4 +0,0 @@
-var how_much;
-if(how_much = prompt('how much', 100)) {
-  $('.left .unfollow:visible').slice(-how_much).click();
-}
--- bookmarklets/unfollow.fr/unfollow_not_following_back.js
+++
@@ -1,4 +0,0 @@
-var how_much;
-if(how_much = prompt('how much', 100)) {
-  $('.right .unfollow:visible').slice(-how_much).click();
-}
