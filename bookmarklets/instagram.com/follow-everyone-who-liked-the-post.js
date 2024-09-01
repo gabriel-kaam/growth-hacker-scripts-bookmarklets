86efba730e3bda7590381952f64cd2971ecb81bc
Gabriel Kaam <gabriel@kaam.fr>
2024-09-01 21:30:03 +0200

    WIP - instagram.com/follow-everyone-who-liked-the-post.js

--- bookmarklets/instagram.com/follow-everyone-who-liked-the-post.js
+++ bookmarklets/instagram.com/follow-everyone-who-liked-the-post.js
@@ -1,20 +1,31 @@
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
 function getDiv() {
-  return document.querySelector('._ab8w._ab94._ab99._ab9f._ab9m._ab9o._ab9s').firstChild;
+  var div = document.querySelector('._ab8w._ab94._ab99._ab9f._ab9m._ab9o._ab9s');
+  return div ? div.firstChild : null;
 }
 
-if(!getDiv()) {
-  document.querySelector('._aacl._aaco._aacw._aacx._aada._aade').click();
+function getNextLink() {
+  return document.querySelector('._acan._acap._acas');
 }
+
+while(!(div = getDiv())) {
+  console.debug('Popup was not found. Opening then sleeping for 2 secs...')
+  document.querySelector('._aacl._aaco._aacw._aacx._aada._aade').click();
+  await sleep(2000);
+}
+console.debug('Popup was found. Resuming...')
 
 var i = 0; var j = 0;
-var div = getDiv();
 
-div.scrollTop = div.scrollHeight;
+do {
+  link = getNextLink();
 
-document.querySelectorAll('._acan._acap._acas').forEach((e) => {
-  setTimeout(function() {
-    div.scrollTop = div.scrollHeight;
-    e.click();
-    console.log(`Followed ${++j}/${i}: done`);
-  }, ++i*2000);
-});
+  console.debug('Link is', link);
+
+  link.click();
+  console.log(`Followed ${++j}/${i}: done`);
+
+  div.scrollTop = div.scrollHeight;
+  await sleep(2000);
+} while(link);
