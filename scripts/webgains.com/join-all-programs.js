241731655d4b6ebcece08159f4f1f728505e3053
Gabriel Kaam <gabriel@kaam.fr>
2024-09-21 13:46:28 +0200

    webgains.com/join-all-programs-on-the-page.js -> webgains.com/join-all-programs.js

--- scripts/webgains.com/join-all-programs.js
+++ scripts/webgains.com/join-all-programs.js
@@ -3,6 +3,7 @@ var qs = document.querySelector.bind(document);
 var TIME_TO_WAIT = {
   FOR: {
     PROGRAM_WINDOW_TO_OPEN: 1500,
+    NEW_PAGE_TO_LOAD: 1500,
   },
   AFTER: {
     JOINING_A_PROGRAM: 2000,
@@ -10,7 +11,23 @@ var TIME_TO_WAIT = {
 }
 
 function join_one_program() {
-  qs('[data-testid="join-button"]').click();
+  var join_button = qs('[data-testid="join-button"].test');
+
+  if(!join_button) {
+    var next_page_button = qs('.ant-pagination-next');
+
+    if(!next_page_button) {
+      alert('DONE !');
+      return;
+    }
+
+    next_page_button.click();
+    setTimeout(join_one_program, TIME_TO_WAIT.AFTER.NEW_PAGE_TO_LOAD);
+
+    return;
+  }
+
+  join_button.click();
 
   setTimeout(function() {
     qs('.css-1bmdmec-center input').click();
